c1d6efd4a9cfef05e8834f829afe21d77e3a8847
NATALIA ESPINOZA <ninoska133333@gmail.com>
2021-06-20 15:54:18 -0500

    ultima modificacion antes de la entrega   20-06-2021

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,69 @@
-const cipher = {
-  // ...
+
+const cipher =
+{
+  encode: function (contrasena,palabra) 
+  {
+    //console.log(palabra);
+
+    if(palabra==null || contrasena==0 || palabra==0 || contrasena==null)
+    {
+      throw new TypeError("no puede ser nulo");
+    }
+        
+    let cifrado ="";
+
+    for (var i = 0; i <= palabra.length; i++)
+    {
+      let numContrasena=Number(contrasena);
+      let indice = palabra.charCodeAt(i);
+
+      if (indice==32)
+      {
+      let cifradoEspacio=32;
+      cifrado += String.fromCharCode(cifradoEspacio);
+      }
+
+      else 
+      {
+      cifrado += String.fromCharCode((indice-65+ numContrasena)%26+65);     
+      }
+      //console.log(cifrado);
+
+    }
+    return cifrado;
+  },
+
+  decode: function (contrasena,palabra) 
+  {
+    if(palabra==null || contrasena==0 || palabra==0 ||  contrasena==null)
+      {
+      throw new TypeError("no puede ser nulo");
+      }
+
+      let decifrado = "";
+
+    for (var i = 0; i <= palabra.length; i++) 
+     {
+      let numContrasena=Number(contrasena);  
+      let indice = palabra.charCodeAt(i);
+      if (indice==32)
+        {
+        let cifradoEspacio=32;
+        decifrado += String.fromCharCode(cifradoEspacio);
+        }
+    
+        else 
+        {
+        decifrado += String.fromCharCode((indice+65-numContrasena)%26+65);     
+      // console.log(decifrado);
+        }
+      }
+      return decifrado;
+
+  }
 };
 
+
 export default cipher;
+
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+//console.log(cipher);
+
+alert("si funciona");
+
+// boton encriptar
+
+function textoCifrado ()
+{
+//capturar valor de texto
+let palabra=(document.getElementById("texto").value).toUpperCase();
+
+//capturar valor de contraseña texto
+let contrasena=document.getElementById("contraseña").value;
+//console.log(contrasena);
+
+//ejecuta la funcion con la palabra capturado en cipher.js
+let cifrado=cipher.encode(contrasena,palabra);
+//console.log(cifrado);
+
+//mostrar en pantalla
+document.getElementById("resultado").innerHTML= cifrado;
+}
+
+const botonCipher=document.getElementById("cipher");
+botonCipher.addEventListener("click",textoCifrado);
+
+
+//boton desencriptar
+
+function textoDescifrado () 
+ {
+  //capturar valor de texto
+  let palabra=(document.getElementById("texto").value).toUpperCase();
+  
+  //capturar valor del texto de contraseña
+  let contrasena=document.getElementById("contraseña").value;
+  //console.log(contrasena);
+
+  //ejecuta la funcion con la palabra capturado en en cipher,js
+  let decifrado=cipher.decode(contrasena,palabra);
+  //console.log(decifrado);
+  
+  //mostrar en pantalla
+  document.getElementById("resultado").innerHTML= decifrado;
+  }
+
+const botonDeCipher=document.getElementById("decipher");
+botonDeCipher.addEventListener("click",textoDescifrado);
+
+
